3d3da256282adf13d0d714fef1abba5a51066eda
Keith Ma <95742118+imkeithyeet@users.noreply.github.com>
2023-03-22 20:01:12 -0700

    posts

--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,5 @@
-const express = require('express')
-const router=express.Router()
+const express  = require('express')
+const router = express.Router()
 const Post = require('../models/Post')
 
 
@@ -7,7 +7,18 @@ router.get('/',(req,res)=> {
     res.send('We are on posts')
 })
 router.post('/', (req,res)=>{
-    console.log(req.body)
+    const post = new Post({
+        title: req.body.title,
+        description: req.body.description   
+    })
+
+    post.save()
+    .then(data => {
+        res.json(data)
+    })
+    .catch(err=>{
+        res.json({message:err})
+    })
 })
 
 module.exports = router
